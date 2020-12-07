7dffe323c30eea23fa62fde5d4da158df3a70db7
Thomas Koentges <thomaskoentges@brucheion.io>
2020-12-07 22:22:00 +1300

    badgerTest

--- main.go
+++ main.go
@@ -5,18 +5,27 @@ import (
 	"net/http"
 
 	"github.com/dgraph-io/badger"
-	"github.com/go-chi"
-	"github.com/go-chi/middleware"
+	"github.com/go-chi/chi"
+	"github.com/go-chi/chi/middleware"
 	"github.com/go-chi/render"
 )
 
 var db *badger.DB
 
+type JSONLD struct {
+	Text Text `json:"@context"`
+}
+
+type Text struct {
+	Id   string `json:"id"`
+	Text string `json:"text"`
+}
+
 const (
-	dbPath = "./tmp/postoffice"
+	dbPath = "./postoffice"
 )
 
-func Routes() *chi.Mux {
+func routes() *chi.Mux {
 	router := chi.NewRouter()
 	router.Use(
 		render.SetContentType(render.ContentTypeJSON),
@@ -26,9 +35,17 @@ func Routes() *chi.Mux {
 		middleware.Recoverer,
 	)
 
-	router.Route("v1", func(r chi.Router)) {
-		r.Mount()
-	}
+	router.Route("/v1", func(r chi.Router) {
+		r.Mount("/api/mailbox/", apiRoutes())
+	})
+	return router
+}
+
+func apiRoutes() *chi.Mux {
+	router := chi.NewRouter()
+	router.Get("/{mailbox}", getLDN)
+	router.Post("/{mailbox}", postLDN)
+	return router
 }
 
 func postLDN(w http.ResponseWriter, r *http.Request) {
@@ -42,21 +59,36 @@ func postLDN(w http.ResponseWriter, r *http.Request) {
 }
 
 func getLDN(w http.ResponseWriter, r *http.Request) {
-	err := db.View(func(txn *badger.Txn) error {
-		// Your code here…
-		return nil
-	})
-	if err != nil {
-		log.Fatal(err)
+	mailbox := chi.URLParam(r, "mailbox")
+	// err := db.View(func(txn *badger.Txn) error {
+	// 	// Your code here…
+	// 	return nil
+	// })
+	// if err != nil {
+	// 	log.Fatal(err)
+	// }
+	jsonld := JSONLD{Text: Text{
+		Id:   mailbox,
+		Text: "Hello world",
+	},
 	}
+	render.JSON(w, r, jsonld)
 }
 
 func main() {
-	router := Routes()
+	router := routes()
 	db, err := badger.Open(badger.DefaultOptions(dbPath))
 	if err != nil {
 		log.Fatal(err)
 	}
 	defer db.Close()
+
+	walkFunc := func(method string, route string, handler http.Handler, middlewares ...func(http.Handler) http.Handler) error {
+		log.Printf("%s %s\n", method, route)
+		return nil
+	}
+	if err := chi.Walk(router, walkFunc); err != nil {
+		log.Panicf("Logging err: %s\n", err.Error())
+	}
 	log.Fatal(http.ListenAndServe(":8080", router))
 }
